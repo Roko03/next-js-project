a5dd9ce966b4ae19a56609275daacd09f6b2c4ee
Roko <rokoponjarac2@gmail.com>
2023-05-18 17:02:32 +0200

    first commit

--- app/Head.jsx
+++
@@ -1,9 +0,0 @@
-export default function Head(){
-    return (
-        <>
-            <title>Next JS App</title>
-            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        </>
-    )
-}
